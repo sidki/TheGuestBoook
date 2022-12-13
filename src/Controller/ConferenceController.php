3c5d6fcd95f77e449bb55a59670c7f8cbcac32e2
Sidki KBOUBI <skboubi@MacBook-Pro-de-Sidki.local>
2022-12-13 22:15:00 +0100

    Comment and conference entities added

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,17 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $greet = '';
+        if ($name = $request->query->get('hello')){
+            $greet = sprintf('<h1> hello %s!</h1>', htmlspecialchars($name));
+        }
         return new Response(
             <<<EOF
                 <html>
                     <body>
+                    $greet
                     <img src="/images/under-construction.gif" />
                     </body>
                 </html>
